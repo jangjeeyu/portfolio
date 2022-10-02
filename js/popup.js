d397d62a187a0ed8b9f4407dd60d6fc971f6fb40
jangjeeyu <112834585+jangjeeyu@users.noreply.github.com>
2022-10-02 21:43:47 +0900

    1002

--- js/popup.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('#pop_up .close').click(function(){
-      $('#pop_up').fadeOut(200)
-    });
-    $('article button').click(function(){
-      $('#pop_up').fadeIn(200);
-    })
-})
